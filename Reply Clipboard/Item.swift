ee48835f0b181fe38d6451c433eb5d869251e618
Andrew Forget <andrew.forget@andydragon.com>
2024-02-18 15:50:46 -0500

    Minor fix

--- Reply Clipboard/Item.swift
+++ Reply Clipboard/Item.swift
@@ -13,7 +13,7 @@ final class Item {
     @Transient var id: UUID = UUID()
     var name: String = ""
     var text: String = ""
-    
+
     init(name: String, text: String) {
         self.name = name
         self.text = text
@@ -27,33 +27,33 @@ final class Item {
 }
 
 class CodableItem: Codable {
-    @Transient var id: UUID = UUID()
+    var id: UUID = UUID()
     var name: String = ""
     var text: String = ""
-    
+
     init(_ item: Item) {
         self.id = item.id
         self.name = item.name
         self.text = item.text
     }
-    
+
     func toItem() -> Item {
         return Item(id: id, name: name, text: text)
     }
-    
+
     enum CodingKeys: CodingKey {
         case id
         case name
         case text
     }
-    
+
     required init(from decoder: Decoder) throws {
         let container = try decoder.container(keyedBy: CodingKeys.self)
         id = try container.decodeIfPresent(UUID.self, forKey: .id) ?? UUID()
         name = try container.decode(String.self, forKey: .name)
         text = try container.decode(String.self, forKey: .text)
     }
-    
+
     func encode(to encoder: Encoder) throws {
         var container = encoder.container(keyedBy: CodingKeys.self)
         try container.encode(id, forKey: .id)
